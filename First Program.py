799aaeb90249da3a9876922b299296b052cbae7a
Rachit <rachitnagwani13@gmail.com>
2018-01-17 20:58:33 +0530

    Initial commit

---
+++ First Program.py
@@ -0,0 +1,5 @@
+import tkinter
+root = tkinter.Tk()
+theLabel = tkinter.Label(root, text="Hey this is the first program")   #to make a label in the window
+theLabel.pack()    #to pack the label the window anywhere in window
+root.mainloop()    #to keep the window running until we don't close it

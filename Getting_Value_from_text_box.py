9967cabc7a882d9851ba95584f71ca6ee4040d60
Rachit <rachitnagwani13@gmail.com>
2018-01-23 14:11:19 +0530

    Fourth Commit

---
+++ Getting_Value_from_text_box.py
@@ -0,0 +1,23 @@
+import tkinter
+
+root =tkinter.Tk()
+name=tkinter.Label(root,text="Name")
+age=tkinter.Label(root,text="Age")
+Entry1=tkinter.Entry(root)
+Entry2=tkinter.Entry(root)
+
+
+def retrieve():
+    input1=Entry1.get()
+    input2=Entry2.get()
+    print("the person's name is"+input1+"and his age is"+input2)
+
+
+but=tkinter.Button(root,text="Submit",command=retrieve)
+name.grid(row=0)
+age.grid(row=1)
+Entry1.grid(row=0,column=1)
+Entry2.grid(row=1,column=1)
+but.grid(row=2)
+root.mainloop()
+
